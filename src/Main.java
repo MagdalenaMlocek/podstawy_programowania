b48c6afdb0109c15ba333d3ca0d57c3b5ac09bcd
Magda <magdalena.mlocek20@gmail.com>
2021-11-07 14:51:36 +0100

    Learning basic of java

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,51 @@
+import java.util.Scanner;
+
 public class Main {
             public static void main(String[] args) {
-                int liczba = 5;
-                int liczba2;
-                liczba2 = 2;
-//                System.out.println("Hello word" + liczba);
+//                int liczba = 5;
+//                int liczba2;
+//                liczba2 = 2;
+////                System.out.println("Hello word" + liczba);
+////
+////
+////                System.out.println(liczba % liczba2);
+////
+////                String name = " Magda";
+////                int age = 25;
+////                System.out.println("My name is" + name + " and I have " + age + " years old." );
 //
 //
-//                System.out.println(liczba % liczba2);
+//                String account = "Magda Mlocek";
+//                account.length();
+//                System.out.println(account.length());
 //
-//                String name = " Magda";
-//                int age = 25;
-//                System.out.println("My name is" + name + " and I have " + age + " years old." );
+//                System.out.println(account.indexOf('r'));
+//                System.out.println(account.substring(1,6));
+//                System.out.println(account.trim());
+//                System.out.println(account.length());
+//
+//                char a = 'a';
+//                char alpha = '\u03B1';
+//                System.out.println(alpha);
+//
+//                char[] witaj = {'w','i','t','a','j'};
+//                System.out.println(witaj);
+//                Character.isDigit(a);
+//                System.out.println( Character.isDigit(a));
+//
+//                if(liczba>liczba2 | liczba2==liczba){
+//                    System.out.println("SystemDziala");
+//                }else {
+//                    System.out.println("SystemNieDziala");
+//                    if (liczba==liczba2){
+//                        System.out.println("SystemDziala1");
+//                    }
+//                }
 
+                Scanner skaner = new Scanner(System.in);
+                String userImput = skaner.nextLine();
+                System.out.println(userImput);
 
-                String account = "Magda Mlocek";
-                account.length();
-                System.out.println(account.length());
-
-                System.out.println(account.indexOf('r'));
-                System.out.println(account.substring(1,6));
-                System.out.println(account.trim());
-                System.out.println(account.length());
-
-                char a = 'a';
-                char alpha = '\u03B1';
-                System.out.println(alpha);
-
-                char[] witaj = {'w','i','t','a','j'};
-                System.out.println(witaj);
-                Character.isDigit(a);
-                System.out.println( Character.isDigit(a));
-
-                if(liczba>liczba2 | liczba2==liczba){
-                    System.out.println("SystemDziala");
-                }else {
-                    System.out.println("SystemNieDziala");
-                    if (liczba==liczba2){
-                        System.out.println("SystemDziala1");
-                    }
-                }
 
     }
 
